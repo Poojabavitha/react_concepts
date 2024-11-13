69032996a830f52515a86aacc381d675c9ad7d80
Poojabavitha <bavithashanmugam19@gmail.com>
2024-11-13 22:29:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bulma/css/bulma.css'
 import './App.css';
+import ProfileCard from './components/ProfileCard';
+import alexaimage from './images/alexa.png';
+import cortanaimage from './images/cortana.png';
+import siriimage from './images/siri.png';
+
 
 function App() {
+
+  
+// const date = new Date();
+// const time = date.toLocaleTimeString();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <section class="hero is-primary">
+  <div class="hero-body">
+    <p class="title">Personal Digital Assitants</p>
+    
+  </div>
+</section>
+      
+      <div className='container'>
+        <section className='section'>
+          <div className='columns'>
+            <div className='column is-3'>
+            <ProfileCard title="Alexa"
+             handle="@alexa99" 
+             image={alexaimage}
+             description="Alexa was created by Amazon used to buy things."/>
+            </div>
+            <div className='column is-3'>
+            <ProfileCard title="Cortana" 
+            handle="@cortana32"
+             image={cortanaimage}
+             description="Cortana was created by Microsoft ."/>
+            </div>
+            <div className='column is-3'>
+            <ProfileCard title="Siri"
+             handle="@siri01" 
+             image={siriimage}
+             description="Siri was created by Apple is phased out"/>
+            </div>
+          </div>
+        </section>
+      </div>
+      
+      
+      
     </div>
+   
   );
 }
 
