d1c44843d0b6f1fa754f3ce5d7174b928a8cf58b
Poojabavitha <bavithashanmugam19@gmail.com>
2024-12-07 08:23:41 +0530

    initial commit

---
+++ 20 Slices with redux toolkit/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import CarForm from './components/CarForm';
+import CarValue from './components/CarValue';
+import CarSearch from './components/CarSearch';
+import CarList from './components/CarList';
+
+
+
+
+function App() {
+  return (
+    <div className="container is-fluid">
+        <CarForm/>
+        <CarSearch/>
+        <CarList/>
+        <CarValue/>
+    </div>
+  );
+}
+
+export default App;
