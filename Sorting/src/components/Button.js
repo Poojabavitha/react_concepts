09be418c31bc57b633a1d269d0392ee45335dbe7
Poojabavitha <bavithashanmugam19@gmail.com>
2024-12-07 13:52:52 +0530

    second commit

---
+++ Sorting/src/components/Button.js
@@ -0,0 +1,64 @@
+import React from "react";
+// import PropTypes from 'prop-types';
+import className from 'classnames';
+
+// const finalClassName= className({
+// 'bg-blue-500': true,
+
+// });
+
+
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+  onClick,
+  ...rest
+}) {
+    const classes = className( rest.className ,' flex items-centre px-3 py-1.5 border',{
+        'border-blue-500 bg-blue-500 text-white': primary,
+        'border-gray-900 bg-gray-900 text-white': secondary,
+        'border-yellow-500 bg-yellow-500 text-white': warning,
+        'border-red-500 bg-red-500 text-white': danger,
+        'border-green-500 bg-green-500 text-white': success,
+        'rounded-full': rounded,
+        'bg-white': outline,
+        'text-blue-500': outline && primary,
+        'text-gray-500': outline && secondary,
+        'text-green-500': outline  && success,
+        'text-yellow-500': outline  && warning,
+        'text-red-500': outline && danger,
+    });
+
+
+
+  return (
+    <button {...rest}  className={classes}>
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!warning) +
+      Number(!!success) +
+      Number(!!danger);
+
+    if (count > 1) {
+      return new Error(
+        'Only one of primary, secondary, success, warning, danger can be true'
+      );
+    }
+  },
+};
+
+export default Button;
---
+++ Sorting/src/pages/ModalPage.js
@@ -0,0 +1,47 @@
+import Modal from '../components/Modal';
+// import Button from '../components/Button';
+import { useState } from 'react';
+
+function ModalPage (){
+const [showModal, setShowModal]=useState(false);
+
+const handleClick =()=>{
+    setShowModal(true);
+    console.log('click');
+    };
+
+    const handleClose=()=>{
+ setShowModal(false);
+    };
+
+    const actionBar =<div>
+        <button  onClick={handleClose} style={{backgroundColor:'#007bff', color:"white",borderRadius:"0.25rem", width:"100px", height:"30px"}}>I accept</button>
+    </div>
+
+    const modal = <Modal onClose={handleClose}
+    actionBar={actionBar}>
+        <p>
+            Here is an important agrrement for you to accept.
+        </p>
+    </Modal>
+
+return(
+    <div className='relative'>
+        
+       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    
+       <button onClick={handleClick} style={{backgroundColor:'#007bff', color:"white",borderRadius:"0.25rem", width:"100px", height:"30px"}} > open modal</button>
+       {showModal && modal}
+    
+    </div>
+
+)
+}
+
+export default ModalPage;
